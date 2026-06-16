30d7a97f74c090c55ed22d2fed66a893bd3a44c9
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 17:28:37 -0500

    Added vitest code coverage suite

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    testTimeout: 10000, 
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['cobertura'],
+      reportsDirectory: './coverage'
+    }
+  }
+});
